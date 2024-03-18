a2f3a21ebc7e311d34caec37855d8b07b9790d3c
Ivan Yenin <cs_67@mail.ru>
2024-03-18 14:45:57 +0300

    Добавлена часть кода с циклом, который проверяет длинну символов массива.

--- Program.cs
+++ Program.cs
@@ -11,3 +11,14 @@ class Program
         Console.WriteLine("Array: [" + string.Join(", ", originalArray) + "]");
 
         string[] result = new string[0];
+
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            if (originalArray[i].Trim().Length <= 3)
+            {
+                string[] temp = new string[result.Length + 1];
+                Array.Copy(result, temp, result.Length);
+                temp[result.Length] = originalArray[i];
+                result = temp;
+            }
+        }
